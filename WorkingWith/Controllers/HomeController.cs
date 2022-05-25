eac2a8e54709f653b0fe8b850b54f1f561ec2a7f
Sasha1448 <agames1448@gmail.com>
2022-05-25 21:44:42 +0300

    [Added] controller

---
+++ WorkingWith/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkingWith.Models;
+
+namespace WorkingWith;
+
+public class HomeController : Controller
+{
+    public IActionResult Index() => View(SimpleRepository.ShardRepositiry.Products);
+}
